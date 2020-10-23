296147b41ed66760e210cf419aadeae5047f5880
Yuri Rech <yuri.nunes@tuntscorp.com>
2020-10-23 18:54:08 +0200

    add fav icon

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
     <div>
       <Head>
         <title>YR - Web Design and Development</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
       </Head>
 
       <main>
