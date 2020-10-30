b0893916fc95eee048ed279b0fea43e793999ad8
Yuri Rech <yuri.nunes@tuntscorp.com>
2020-10-30 19:34:43 +0100

    authentication finished

--- firebase/firebase.utils.tsx
+++ firebase/firebase.utils.tsx
@@ -1,7 +1,8 @@
 import * as firebase from "firebase/app";
+import 'firebase/auth';
 import 'firebase/storage';
-//Adding firestore
 import 'firebase/firestore';
+
 
 export const firebaseConfig = {
   apiKey: "AIzaSyBf8vVNPwTPs1z9Y7KNoWxNVAvFpn15Yr8",
@@ -19,11 +20,12 @@ if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
 }
 
+//Initialize firestore/db
 export const db = firebase.firestore();
 
+//Initialize storage
 const storage = firebase.storage();
 
 export const storageRef = storage.ref();
-export const imagesRef = storageRef.child('35239431.jpg');
-export const imagesOfImagesRef = storageRef.child('images/35239431.jpg');
 
+export const auth = firebase.auth();
